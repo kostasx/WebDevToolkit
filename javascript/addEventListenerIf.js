b3cfd365ba794d833ea7881d8cecf1757d968bf2
Kostas Minaidis <kostas@creativecollective.gr>
2016-03-02 22:32:59 +0100

    Adding addEventListenerIf helper function

---
+++ javascript/addEventListenerIf.js
@@ -0,0 +1,47 @@
+if ( typeof addEventListenerIf === "undefined" ){
+
+  var addEventListenerIf = function( selector, type, listener, useCapture ) {
+
+      var element = ( typeof selector === "string" )? document.querySelectorAll(selector) : selector;
+
+      if ( element && element instanceof Element ){
+
+        return Element.prototype.addEventListener.call(element, type, listener, useCapture);  
+
+      } else if ( element.constructor.name === 'NodeList' ) {
+
+        Array.prototype.slice.call(element).map(function(element){
+
+          Element.prototype.addEventListener.call(element, type, listener, useCapture);
+
+        });
+
+      }
+
+  };  
+
+}
+
+/*
+
+Usage:
+
+(1)
+
+addEventListenerIf( '.js-add-mail-link', 'click', function(e){
+    e.preventDefault();
+    document.querySelector("#new-email-dialog").open()
+});
+
+(2)
+
+var mailLink = document.querySelector('.js-add-mail-link');
+
+addEventListenerIf( mailLink, 'click', function(e){
+    e.preventDefault();
+    document.querySelector("#new-email-dialog").open()
+});
+
+References: addEventListenerPolyfill: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+
+*/
